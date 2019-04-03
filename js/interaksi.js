80871b80d7ceb6bdcf22a6949b2cc16516879ecf
M. Rizqi Irfandiansyah <rizqiirfan23@gmail.com>
2019-04-03 17:06:42 +0700

    update responsive

--- js/interaksi.js
+++ js/interaksi.js
@@ -1,5 +1,6 @@
 function buka(kelas, pos) {
 	var id = pos.parentNode.parentNode.parentNode.parentNode.id;
+	tutup_navbar('content-'+id);
 	tutup('.'+id);
 	document.querySelector(kelas).classList.add('show')
 }
@@ -10,5 +11,20 @@ function tutup(kelas){
 	let selektor = document.querySelector(kelas);
 	if (selektor.classList.contains('show')) {
 		selektor.classList.remove('show');
+	}
+}
+function buka_navbar(pos){
+	var id = pos.parentNode.parentNode.id;
+	var selektorId = document.getElementById(id);
+	if (selektorId.classList.contains('show-nav')) {
+		selektorId.classList.remove('show-nav');
+	} else {
+		selektorId.classList.add('show-nav')
+	}
+}
+function tutup_navbar(id){
+	var selektorId = document.getElementById(id);
+	if (selektorId.classList.contains('show-nav')) {
+		selektorId.classList.remove('show-nav');
 	}
 }
